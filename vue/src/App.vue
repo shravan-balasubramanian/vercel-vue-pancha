bbf06fabe63d4c3ac1e29bbcebf6f1d49c425bc1
shravan-balasubramanian <shravan.balasubramanian@freshworks.com>
2021-03-23 07:50:28 +0530

    Added functionalities

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,22 +1,94 @@
 <template>
   <div id="app">
-    Hello Vercel
+    <div class="header">
+      <form @submit.prevent="getSearchMovies">
+        <input type="text" v-model="search" />
+        <button type="submit">Go</button>
+      </form>
+    </div>
+    <div class="results">
+      <div v-for="result in movieResults" :key="result.title" class="result">
+        <div class="image">
+          <img :src="result.Poster" alt="Poster" />
+        </div>
+        <div class="details">
+          <div class="title">{{ result.Title }}</div>
+          <div class="year">{{ result.Year }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-}
+  name: "app",
+  data() {
+    return {
+      movieResults: [],
+      search: ""
+    };
+  },
+  methods: {
+    async getSearchMovies() {
+      if (this.search.length > 0) {
+        const url = `http://omdbapi.com?s=${this.search}&apikey=68dad405`;
+        this.movieResults = (await (await fetch(url)).json()).Search.splice(
+          1,
+          5
+        );
+      }
+    }
+  }
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .header {
+    display: flex;
+    justify-content: center;
+  }
+  .results {
+    margin-top: 20px;
+    margin-left: 40%;
+    .result {
+      box-sizing: border-box;
+      margin-top: 20px;
+      border-radius: 5px;
+      width: 50%;
+      height: 100px;
+      background: azure;
+      display: flex;
+      border: 2px solid lightblue;
+      .image {
+        width: 40%;
+        height: 100px;
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      }
+      .details {
+        text-align: center;
+        margin-left: 10px;
+        color: gray;
+        .title {
+          font-size: 15px;
+          font-weight: 800;
+        }
+        .year {
+          font-weight: 600;
+        }
+      }
+    }
+  }
 }
 </style>
