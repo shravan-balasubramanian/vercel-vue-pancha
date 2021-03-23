caf725c826766dbefb908be0d3b695245efa86af
shravan-balasubramanian <shravan.balasubramanian@freshworks.com>
2021-03-23 09:57:31 +0530

    changed log

--- vue/api/main.js
+++ vue/api/main.js
@@ -1,14 +1,21 @@
 const axios = require('axios');
 
 const exportFunction = async (req, res) => {
-  // eslint-disable-next-line no-console
-  console.log('User is searching for the movie: ' + s);
-  const { url, apikey, s } = req.query;
-  const response = await axios.get(`${url}?s=${s}&apikey=${apikey}`);
+  try {
+    const { url, apikey, s } = req.query;
+    const response = await axios.get(`${url}?s=${s}&apikey=${apikey}`);
 
-  res.json({
-      result: response.data,
-  })
+    // eslint-disable-next-line no-console
+    console.log('User is searching for the movie: ' + s);
+
+    res.json({
+        result: response.data,
+    });
+  } catch (ex) {
+    res.status(ex.response.status).json({
+      error: `Failed with ${JSON.stringify(ex)}`
+    });
+  }
 };
 
 module.exports = exportFunction;
